8cb89661c78e0de7ad7953c3d440237e717aa378
naor <naor2205@gmail.com>
2020-05-08 12:26:15 +0300

    add minor config

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 app-api: PORT=$APP_API_PORT nodemon services/app-service/index.js
-messaging-api: PORT=$MESSAGES_API_PORT services/nodemon messaging-service/index.js
+messaging-api: PORT=$MESSAGES_API_PORT nodemon services/messaging-service/index.js
 video-api: PORT=$VIDEO_PORT nodemon services/video-service/index.js
 app-ui: cd app-ui && npm start
--- services/messaging-service/index.js
+++ services/messaging-service/index.js
@@ -3,8 +3,6 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT;
 
-const initLogger = require('../utils/logger/winston');
-const logger = initLogger(`${__dirname}`, "messaging");
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Messaging service -  http://localhost:${port}`));
